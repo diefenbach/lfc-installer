5030ce18d0804d6291bc62472c97f6891f3a881c
Kai Diefenbach <kai.diefenbach@iqpp.de>
2014-06-26 21:29:23 +0200

    Updates settings.

--- lfc_project/settings.py
+++ lfc_project/settings.py
@@ -1,131 +1,191 @@
 import os
+
 DIRNAME = os.path.dirname(__file__)
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
+COMPRESS_ENABLED = False
+COMPRESS_CACHE_BACKEND = 'locmem:///'
+
+DEFAULT_FROM_EMAIL = 'your_email@domain.com'
 
 ADMINS = (
-    # ('name', 'email'),
+    # ('Your Name', 'your_email@domain.com'),
 )
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = ''      # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = ''      # Or path to database file if using sqlite3.
-DATABASE_USER = ''         # Not used with sqlite3.
-DATABASE_PASSWORD = ''         # Not used with sqlite3.
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.',   # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '',                        # Or path to database file if using sqlite3.
+        'USER': '',                        # Not used with sqlite3.
+        'PASSWORD': '',                    # Not used with sqlite3.
+        'HOST': '',                        # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                        # Set to empty string for default. Not used with sqlite3.
+    }
+}
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'Europe/Berlin'
+TIME_ZONE = 'UTC'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'en'
+
 SITE_ID = 1
 
 # If you set this to False, Django will make some optimizations so as not
 # to load the internationalization machinery.
 USE_I18N = True
 
-# Absolute path to the directory that holds media.
-# Example: "/home/media/media.lawrence.com/"
+# media files settings
+MEDIA_URL = '/media/'
 MEDIA_ROOT = DIRNAME + "/media"
 
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash if there is a path component (optional in other cases).
-# Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = '/media/'
-
-# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
-# trailing slash.
-# Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/media/admin/'
+# static files settings
+STATIC_URL = '/static/'
+STATIC_ROOT = DIRNAME + "/sitestatic"
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = '+0zsw5n@v7*rhl6r6ufqhoc6jlqq0f-u8c+gh(hjb+_jmg@rh6'
 
-# List of callables that know how to import templates from various sources.
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.load_template_source',
-    'django.template.loaders.app_directories.load_template_source',
-#     'django.template.loaders.eggs.load_template_source',
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'compressor.finders.CompressorFinder',
 )
 
 MIDDLEWARE_CLASSES = (
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.locale.LocaleMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     "pagination.middleware.PaginationMiddleware",
+    'django.contrib.messages.middleware.MessageMiddleware',
     "lfc.utils.middleware.LFCMiddleware",
 )
 
-CACHE_MIDDLEWARE_KEY_PREFIX = "lfc"
 ROOT_URLCONF = 'urls'
 
-TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-)
-
 INSTALLED_APPS = (
+    "compressor",
     "lfc_theme",
     "django.contrib.admin",
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
+    'django.contrib.staticfiles',
     "django.contrib.flatpages",
     "django.contrib.sitemaps",
     "django.contrib.comments",
     "lfc",
+    "lfc_contact_form",
+    "lfc_page",
+    "lfc_portlets",
+    "pagination",
+    "permissions",
     "portlets",
     "tagging",
-    "contact_form",
-    "pagination",
     "workflows",
-    "permissions",
-    "lfc_blog",
 )
 
-CACHE_BACKEND = 'dummy:///'
 
-FORCE_SCRIPT_NAME=""
+FORCE_SCRIPT_NAME = ""
+
 LOGIN_URL = '/login'
 LOGOUT_URL = '/logout'
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/manage/'
 
 TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.debug',
-    'django.core.context_processors.auth',
+    'django.contrib.auth.context_processors.auth',
     'django.core.context_processors.request',
     'django.core.context_processors.media',
+    'django.core.context_processors.static',
     'django.core.context_processors.i18n',
     'lfc.context_processors.main',
 )
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+    }
+}
 
-DEFAULT_FROM_EMAIL = ""
+CACHE_MIDDLEWARE_KEY_PREFIX = 'lfc'
+
 EMAIL_HOST = ""
 EMAIL_HOST_USER = ""
 EMAIL_HOST_PASSWORD = ""
 
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "%(asctime)s %(levelname)s %(message)s",
+            "datefmt": "%a, %d %b %Y %H:%M:%S",
+        },
+        "simple": {
+            "format": "%(levelname)s %(message)s",
+            "datefmt": "%a, %d %b %Y %H:%M:%S",
+        },
+    },
+    "handlers": {
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "formatter": "verbose",
+        },
+        'logfile': {
+            'level': 'DEBUG',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'formatter': 'verbose',
+            'filename': os.path.join(DIRNAME, "..", "lfc.log"),
+            'mode': 'a',
+        },
+    },
+    "loggers": {
+        "default": {
+            "handlers": ["logfile", "console"],
+            "level": "DEBUG",
+            "propagate": False,
+        },
+        "qinspect": {
+            "handlers": ["console"],
+            "level": "DEBUG",
+            "propagate": False,
+        },
+    }
+}
+
 PAGINATION_DEFAULT_PAGINATION = 5
 PAGINATION_DEFAULT_WINDOW = 1
 
-LANGUAGES = (("en", u"English"), ("de", u"German"))
+LANGUAGES = (("en", u"English"), ("de", u"German",))
 LFC_MULTILANGUAGE = len(LANGUAGES) > 1
 LFC_MANAGE_WORKFLOWS = True
 LFC_MANAGE_PERMISSIONS = True
 LFC_MANAGE_APPLICATIONS = True
 LFC_MANAGE_USERS = True
+LFC_MANAGE_UTILS = True
+
+LFC_MANAGE_META_DATA = True
+LFC_MANAGE_SEO = True
+LFC_MANAGE_COMMENTS = True
+LFC_MANAGE_PORTLETS = True
+LFC_MANAGE_IMAGES = True
+LFC_MANAGE_FILES = True
+
+LFC_TAGS = [
+    "lfc_tags",
+]
 
 try:
     from local_settings import *
