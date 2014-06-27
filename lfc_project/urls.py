43b167449e101781141cfa00e1c3a267be05fd6b
Kai Diefenbach <kai.diefenbach@iqpp.de>
2014-06-27 10:53:42 +0200

    Cleans up urls.py.

--- lfc_project/urls.py
+++ lfc_project/urls.py
@@ -1,25 +1,16 @@
-# python imports
-import os
-
 # django imports
 from django.conf import settings
-from django.conf.urls import include, patterns, url
+from django.conf.urls import include, patterns
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.contrib.auth.views import login, logout
 
 admin.autodiscover()
-DIRNAME = os.path.dirname(__file__)
-
 handler500 = 'lfc.views.fiveohoh'
 
 # Django
 urlpatterns = patterns('',
-    url('^accounts/login/?$', login, {"template_name": "admin/login.html"}, name='auth_login'),
-    url('^accounts/logout/?$', logout, name='auth_logout'),
     (r'^admin/', include(admin.site.urls)),
-    (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
-    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT, 'show_indexes': True}),
-)
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # LFC
 urlpatterns += patterns('',
